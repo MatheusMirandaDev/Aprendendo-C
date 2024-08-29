684b4eab86615068967f2f5929f336fdff41a05a
Matheus Miranda <matheusmiranda.batista@gmail.com>
2024-08-29 08:49:59 -0300

    basico funções

--- main.c
+++ main.c
@@ -1,48 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <locale.h>
-#define t 3
-
-struct tipoPessoa
-{
-    int idade;
-    float peso;
-    char nome[50];
-};
-
-typedef struct tipoPessoa tipoPessoa;
 
 int main()
 {
-    setlocale(LC_ALL, "Portuguese");
+    void imprimaMensagem(void); // declaro a minha função criada lá embaixo
 
-    tipoPessoa lista[t];
-    int i;
-
-    for (i = 0; i < t; i++)
-    {
-        printf("insira os dados da pessoa (%d)\n", i + 1);
-        printf("nome: ");
-        fgets(lista[i].nome, 50, stdin);
-        fflush(stdin);
-
-        printf("Idade:");
-        scanf("%d", &lista[i].idade);
-        fflush(stdin);
-
-        printf("peso:");
-        scanf("%f", &lista[i].peso);
-        fflush(stdin);
-    }
-    system("cls");
-
-    for (i = 0; i < t; i++)
-    {
-        printf("------ Pessoa %d ----------\n", i + 1);
-        printf("Nome: %s\n", lista[i].nome);
-        printf("Idade: %d\n", lista[i].idade);
-        printf("Peso: %.2f\n", lista[i].peso);
-    }
+    imprimaMensagem(); // chamo a minha função criada la embaixo para imprimir oq está contido nela
     return 0;
+}
+
+void imprimaMensagem(void) // criando uma segunda função, ela é vazia e tem argumento vazio
+{
+    printf("teste"); // quero imprmir essa mensagem
 }
